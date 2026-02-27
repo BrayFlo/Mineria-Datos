2dc2993e32c3e9fa4d3871e61b83478a58ae06e1
BrayFlo <151600261+BrayFlo@users.noreply.github.com>
2026-02-26 20:01:15 -0600

    Add files via upload

---
+++ Estadistica/estadisticas.py
@@ -0,0 +1,44 @@
+#Creacion de la clase
+class Estadistica:
+
+    #Metodo Constructor
+    def __init__(self, numeros):
+        self.numeros = numeros
+
+    #metodos en poo para la media
+    def calcular_media(self):
+        suma = sum(self.numeros)
+        cantidad = len(self.numeros)
+        return suma/cantidad
+    
+    #metodo para la mediana
+    def calcular_mediana(self):
+        numeros_ordenados = sorted(self.numeros)
+        n = len(numeros_ordenados)
+
+        mitad = n // 2
+
+        if n %2 == 0:
+            return numeros_ordenados[mitad - 1] + numeros_ordenados[mitad] / 2
+        else:
+            return numeros_ordenados[mitad]
+        
+    #Metodo para calcular la moda
+    def calcular_moda(self):
+        frecuencias = {}
+        for numero in self.numeros:
+            if numero in frecuencias:
+                frecuencias[numero] += 1
+            else:
+                frecuencias[numero] = 1
+
+        max_frecuencia = max(frecuencias.values())
+        modas = []
+        for numero, frecuencia in frecuencias.items():
+            if frecuencia == max_frecuencia:
+                modas.append(numero)
+
+        if len(modas) == len(self.numeros):
+            return "No hay moda"
+        return modas
+        
